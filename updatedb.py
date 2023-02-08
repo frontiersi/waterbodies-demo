1b12d349c9f03e38526b040422116fcfa477b6ac
Caitlin Adams <caitlin.isabel.adams@gmail.com>
2023-02-08 12:00:27 +1100

    Add comments throughout updatedb.py

--- updatedb.py
+++ updatedb.py
@@ -1,3 +1,10 @@
+"""
+Update Database
+
+This script allows the user to connect to a database and return the version
+This is a baseline that will be adapted to have update capability, in conjunction with 
+code to update the necessary rows based on what has changed
+"""
 import click
 import psycopg2
 
@@ -10,6 +17,7 @@ def updatedb(database):
     """
 
     try:
+        # Create a connection to the database
         connection = psycopg2.connect(
             user="postgres",
             password="",
@@ -18,19 +26,25 @@ def updatedb(database):
             database=database,
         )
 
-        cursor = connection.cursor()
-
+        # Display connection information
         print("PostgreSQL server information")
         print(connection.get_dsn_parameters(), "\n")
 
+        # Create a cursor, which will perform data base operations
+        cursor = connection.cursor()
+
+        # Select the version from the database
         cursor.execute("SELECT version();")
 
+        # Return the information held by the cursor and display
         record = cursor.fetchone()
         print(f"You are connected to {record} \n")
 
+    # Should the connection fail, display an error message
     except (Exception, psycopg2.Error) as error:
         print(f"Error while connecting to PostgreSQL: {error}")
 
+    # At the end, if a connection was establsihed, close the cursor and connection
     finally:
         if connection:
             cursor.close()
